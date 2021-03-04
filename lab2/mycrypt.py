0df81bb22b2aefb4eaf1fc497d405fd1751f40d3
Mikkeep <mikke.pennanen@gmail.com>
2021-03-04 15:15:27 +0200

    All test cases pass

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -7,14 +7,13 @@ def encode(s):
     crypted = ""
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     illegalmapping = ["å", "ä", "ö", "+"]
-    for c in s:
-        if c in illegalmapping:
-            raise ValueError
     if len(s) > 1000:
         raise ValueError
     padded_val = s.ljust(1000, "z")
     for c in padded_val:
-        if c.isalpha():
+        if c in illegalmapping:
+            raise ValueError
+        elif c.isalpha():
             if c.islower():
                 c=c.upper()
             # Rot13 the character for maximum security
